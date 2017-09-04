76e2c08dc20f7c33a89864e551943ece921f9d4c
alih107 <alikhan.amandyk@gmail.com>
2017-09-04 19:12:07 +0600

    card2card - debugging 2

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ def reply_typing_off(sender):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
 
 def reply_display_cards(sender, last_sender_message):
-    logging.info('got into reply display cards')
     session = requests.Session()
     headers = {"Authorization": "Basic " + last_sender_message['encodedLoginPass'], 'Content-Type': 'application/json'}
     url_login = 'https://post.kz/mail-app/api/account/'
@@ -93,6 +92,8 @@ def reply_display_cards(sender, last_sender_message):
             }
         }
     }
+    logging.info('trying to send data_cards')
+    logging.info(data)
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data_cards)
     collection_messages.update_one({'sender': sender}, {"$set": last_sender_message}, upsert=False)
 
