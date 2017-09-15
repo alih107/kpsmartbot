5dcbbfb951cc4c6344c78beece357ed9f11ca3f8
alih107 <alikhan.amandyk@gmail.com>
2017-09-15 18:07:37 +0600

    voice_assistant - printing if got voice 5

--- voice_assistant.py
+++ voice_assistant.py
@@ -4,6 +4,8 @@ import logging
 from wit import Wit
 from pydub import AudioSegment
 
+client = Wit('TCLMX5YEBCRG5TO2TLW3VJCOGFOWMOJW')
+
 def handle_voice_message(sender, voice_url):
     try:
         helper.reply(sender, "Я получил аудио-сообщение!")
@@ -13,5 +15,14 @@ def handle_voice_message(sender, voice_url):
         with open(voice_filename, "wb") as o:
             o.write(g.content)
         AudioSegment.from_file(voice_filename, "mp4").export(voice_filename_mp3, format="mp3")
+        with open(voice_filename_mp3, 'rb') as f:
+            try:
+                resp = client.speech(f, None, {'Content-Type': 'audio/mpeg3'})
+                logging.info('Yay, got Wit.ai response: ' + str(resp))
+                if "_text" in resp:
+                    message = resp['_text']
+                logging.info("Message = " + message)
+            except:
+                pass
     except:
         logging.error(helper.PrintException())
