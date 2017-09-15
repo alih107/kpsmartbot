a9f3850853faee7e9908c66d1741b2595e2184bd
alih107 <alikhan.amandyk@gmail.com>
2017-09-15 18:04:32 +0600

    voice_assistant - printing if got voice 5

--- voice_assistant.py
+++ voice_assistant.py
@@ -8,10 +8,10 @@ def handle_voice_message(sender, voice_url):
     try:
         helper.reply(sender, "Я получил аудио-сообщение!")
         g = requests.get(voice_url, stream=True)
-        voice_filename = "voice_" + sender + ".ogg"
+        voice_filename = "voice_" + sender + ".mp4"
         voice_filename_mp3 = "voice_" + sender + ".mp3"
         with open(voice_filename, "wb") as o:
             o.write(g.content)
-        AudioSegment.from_ogg(voice_filename).export(voice_filename_mp3, format="mp3")
+        AudioSegment.from_file(voice_filename, "mp4").export(voice_filename_mp3, format="mp3")
     except:
         logging.error(helper.PrintException())
