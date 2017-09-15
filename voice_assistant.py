f11d4b9cf94bd4fbadc97713bbedc708dee0c001
alih107 <alikhan.amandyk@gmail.com>
2017-09-15 17:29:22 +0600

    voice_assistant - printing if got voice 2

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,6 +1,12 @@
 import helper
+import requests
+import logging
 from wit import Wit
 from pydub import AudioSegment
 
 def handle_voice_message(sender, data):
-    helper.reply(sender, "Я получил аудио-сообщение!")
+    logging.info("Handling audio...")
+    try:
+        helper.reply(sender, "Я получил аудио-сообщение!")
+    except:
+        logging.error(helper.PrintException())
