40560666dcf0ad6dab8d942e4cf86f5afdfc8588
alih107 <alikhan.amandyk@gmail.com>
2017-09-19 11:29:28 +0600

    voice assistant - added info_post

--- voice_assistant.py
+++ voice_assistant.py
@@ -56,63 +56,46 @@ def handle_intent(sender, last_sender_message, value):
     try:
         if value == 'greeting':
             helper.reply(sender, "Здравствуйте, " + last_sender_message['first_name'] + "!")
-            return
         elif value == 'postamat':
             helper.reply(sender, helper.postamat)
-            return
         elif value == 'hybridpost_def':
             helper.reply(sender, helper.hybridpost_def)
-            return
         elif value == 'supermarket':
             helper.reply(sender, helper.what_is_supermarket)
-            return
         elif value == 'trackbynumber_query':
             helper.reply(sender, helper.trackbynumber_query)
-            return
         elif value == 'fastmail_options':
             helper.reply(sender, helper.fastmail_options)
-            return
         elif value == 'postamat_how':
             helper.reply(sender, helper.postamat_how)
-            return
         elif value == 'hybridpost_time':
             helper.reply(sender, helper.hybridpost_time)
-            return
         elif value == 'postamat_info_access':
             helper.reply(sender, helper.postamat_info_access)
-            return
         elif value == 'hybridpost_info':
             helper.reply(sender, helper.hybridpost_info)
-            return
         elif value == 'trackbynumber':
             helper.reply(sender, helper.trackbynumber)
-            return
         elif value == 'redirect':
             helper.reply(sender, helper.redirect)
-            return
         elif value == 'redirect_why':
             helper.reply(sender, helper.redirect_why)
-            return
         elif value == 'package_how_long':
             helper.reply(sender, helper.package_how_long)
-            return
         elif value == 'COMMAND_exchange_rates':
             main.reply_currencies_kursy(sender)
-            return
         elif value == 'COMMAND_go_home':
             main.reply_main_menu_buttons(sender)
-            return
         elif value == 'COMMAND_card2card':
             bot_server.call_card2card(sender, last_sender_message, 'card2card')
-            return
         elif value == 'COMMAND_paymobile':
             bot_server.call_balance(sender, last_sender_message, 'balance')
-            return
         elif value == 'COMMAND_payonai':
             bot_server.call_onai(sender, last_sender_message, 'onai')
-            return
         elif value == 'COMMAND_sendmessage':
             bot_server.call_sendmessage(sender, last_sender_message, 'send.message')
+        elif value == 'info_post':
+            main.reply_tracking(sender, message, last_sender_message)
         else:
             helper.reply(sender, "Я не поняла Вашу команду")
     except:
