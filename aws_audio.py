bd4ffd201c9489cdbab5fb8301b5473f19fd934a
alih107 <alikhan.amandyk@gmail.com>
2017-10-12 14:38:56 +0600

    adding aws_audio.py 6

--- aws_audio.py
+++ aws_audio.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/bot_audio', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
-    print (data)
+    print(data)
     url, topic, source = data['url'], data['topic'], data['source']
 
-    return {'message': 'idi nahui'}, 200
+    return jsonify({'message': 'idi nahui'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
