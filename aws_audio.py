50fb0e1a0693fa908dfd5220986cb05030794015
alih107 <alikhan.amandyk@gmail.com>
2017-10-13 15:41:20 +0600

    aws_audio.py 13

--- aws_audio.py
+++ aws_audio.py
@@ -15,15 +15,6 @@ client = Wit('TCLMX5YEBCRG5TO2TLW3VJCOGFOWMOJW')
 logging.basicConfig(filename='log_audio.log', level=logging.INFO,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 
-ACCESS_TOKEN = "EAAVWwjEHWcYBAOjjLRU8c15mW9ZBKHj9pZAD8k3QmDEZCRyxdN61Jgn79C7PHOZCsyYG0W2lOdkg19VtXQZAIOgRKllVun8KZCFICQ4G2BW8gJVYACAsRznfDExvVvfwyiIq8s5fP0c3c8XHtZAx5EXf4ubxxgAmDSM8GroetIvq5zas3isTEZAn"
-fb_url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN
-
-# test with proxy
-
-def reply_just_text(sender, msg):
-    data = {"recipient": {"id": sender}, "message": {"text": msg}}
-    requests.post(fb_url, json=data)
-
 def PrintException():
     exc_type, exc_obj, tb = sys.exc_info()
     f = tb.tb_frame
